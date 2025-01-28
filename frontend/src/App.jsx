ec43aaf888295d288c09867b38b504c75f041654
sakshyam <sakshyamshrestha111@gmail.com>
2025-01-28 12:06:45 +0545

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Form from "./components/form/Form";
+
 
 const App = () => {
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/form" element={<Form />} /> 
+      </Routes>
+    </Router>
   );
 };
 
